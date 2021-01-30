8e47e472f8c31e68785a0b1f4e4f8c092321d832
Gemuese_Hasser <jonas.lobe.2006@outlook.com>
2021-01-30 17:57:05 +0000

    add missing Javadoc

--- src/main/java/de/jonas/snake/start/ImageLoader.java
+++ src/main/java/de/jonas/snake/start/ImageLoader.java
@@ -6,13 +6,22 @@ import javax.imageio.ImageIO;
 
 import java.awt.Image;
 
+/**
+ * Lädt alle benötigten Bilder, für das {@link StartGUI Start-Fenster}.
+ */
 public final class ImageLoader {
 
-    static Image title;
+    //<editor-fold desc="STATIC-FIELDS">
+    /** Das Titel-Bild. */
+    static Image TITLE;
+    //</editor-fold>
 
+    /**
+     * Erstellt eine neue und vollständig unabhängige Instanz des {@link ImageLoader} und lädt alle Bilder.
+     */
     @SneakyThrows
     public ImageLoader() {
-        title = ImageIO.read(getClass().getResource("/SnakeTitle.jpg"));
+        TITLE = ImageIO.read(getClass().getResource("/SnakeTitle.jpg"));
     }
 
 }
