924adbf25d7061cd00130a89d1c68d1a78acfe8a
Gemuese_Hasser <jonas.lobe.2006@outlook.com>
2021-01-31 20:32:03 +0000

    add game-gui

--- src/main/java/de/jonas/snake/game/GameGUI.java
+++ src/main/java/de/jonas/snake/game/GameGUI.java
@@ -1,9 +1,17 @@
 package de.jonas.snake.game;
 
-public class GameGUI {
+import de.jonas.snake.GUI;
+
+public class GameGUI extends GUI {
+
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+    private static final int CLOSE_OPERATION = 3;
+    private static final String TITLE = "Snake - by Jonas";
 
     public GameGUI() {
-        System.out.println("Starte Game-GUI!");
+        super(WIDTH, HEIGHT, CLOSE_OPERATION, TITLE, false);
+        super.openFrame();
     }
 
 }
